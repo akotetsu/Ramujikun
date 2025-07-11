034ee6e3982e6c80f8b7093ade569ba6334cc692
akotetsu <yuanliqu@gmail.com>
2025-07-11 16:50:22 +0900

    first commit

---
+++ Ramujikun/App/RamujikunApp.swift
@@ -0,0 +1,28 @@
+//
+//  RamujikunApp.swift
+//  Ramujikun
+//
+//  Created by 原里駆 on 2025/07/11.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct RamujikunApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
