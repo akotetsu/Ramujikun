5b9133307d27bae2cce4ef6ad5b26abd48bb9337
akotetsu <yuanliqu@gmail.com>
2025-07-11 16:01:29 +0900

    Initial Commit

---
+++ Ramujikun/RamujikunApp.swift
@@ -0,0 +1,17 @@
+//
+//  RamujikunApp.swift
+//  Ramujikun
+//
+//  Created by 原里駆 on 2025/07/11.
+//
+
+import SwiftUI
+
+@main
+struct RamujikunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
